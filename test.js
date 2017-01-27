4d68739862bfdf8b7fad86b2e2094574310cb853
PJ <moriartp@newschool.edu>
2017-01-26 20:56:06 -0500

    adding script

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
 console.log('hello, world!');
+
+var dataset1 = [1,2,3,4,5];
+
+console.log("string "+"concat");
+
+for( var i = 0; i > Infinity; i++){
+	console.log(i);
+}
+
+for (var i = -10; i <10; i++){
+	console.log(i);
+}
